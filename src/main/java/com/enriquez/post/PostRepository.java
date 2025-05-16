1f193d2a84f0caaf20654ab5b3e87b524f67a8cc
Juzzz10 <justin9953enriquez@gmail.com>
2025-05-16 16:29:59 +0800

    first commit

---
+++ src/main/java/com/enriquez/post/PostRepository.java
@@ -0,0 +1,8 @@
+package com.enriquez.post;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Long> {
+}
---
+++ src/test/java/com/enriquez/post/PostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enriquez.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
